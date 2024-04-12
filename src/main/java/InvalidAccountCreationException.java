c56b9d3bb296ba4ebaf17b203894d3cd22cc7ed5
Spencer Katz <sek49@duke.edu>
2024-04-12 14:42:03 -0400

    Deleted Two Unused Exception Classes

--- src/main/java/InvalidAccountCreationException.java
+++
@@ -1,9 +0,0 @@
-public class InvalidAccountCreationException extends RuntimeException {
-  public InvalidAccountCreationException(String message) {
-    super(message);
-  }
-
-  public InvalidAccountCreationException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
